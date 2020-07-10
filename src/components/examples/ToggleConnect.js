d786af44d916e441a67f1755d87f00f4fe29d529
Evan Flynn <evanflynn.msu@gmail.com>
2020-07-10 12:25:47 -0700

    removed topics list from demo

--- src/components/examples/ToggleConnect.js
+++ src/components/examples/ToggleConnect.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useROS } from 'react-ros'
 
 function ToggleConnect() {
-  const { isConnected, topics, url, changeUrl, toggleConnection } = useROS();
+  const { isConnected, url, changeUrl, toggleConnection } = useROS();
 
   return (
     <div>
@@ -11,8 +11,6 @@ function ToggleConnect() {
         <b>ROS url input:  </b><input name="urlInput" defaultValue={ url } onChange={event => changeUrl(event.target.value)} />  <br />
         <b>ROS url to connect to:  </b> {url}  <br />
         <b>Status of ROS:</b> { isConnected ? "connected" : "not connected" }   <br />
-        <b>Topics detected:</b><br />
-        { topics.map((topic, i) => <li key={i}>    {topic.path}</li> )}
       </p>
     </div>
   );
